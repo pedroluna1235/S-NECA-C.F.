fd000bdbdb10997f516b348231dee5f636c5dd46
Pedro Luna del Rosal <pedroluna1235@gmail.com>
2026-08-27 12:14:09 +0200

    Remove test files

--- test-jspdf.js
+++
@@ -1,8 +0,0 @@
-import { jsPDF } from 'jspdf';
-const doc = new jsPDF();
-try {
-  doc.addImage('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=', 'PNG', 0, 0, 10, 10);
-  console.log('Success');
-} catch (e) {
-  console.error('Error:', e);
-}
--- test-jspdf2.js
+++
@@ -1,9 +0,0 @@
-import { jsPDF } from 'jspdf';
-const doc = new jsPDF();
-try {
-  doc.setFont("helvetica", "italic");
-  doc.text("test", 10, 10);
-  console.log('Success italic');
-} catch (e) {
-  console.error('Error:', e);
-}
